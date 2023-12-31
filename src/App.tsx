b1315707d2e0e549f48f43ded2cbd34cb35e63fc
peymanzarchini <peymanzarchini@outlook.com>
2023-12-31 22:03:24 +0330

    Convert the default styles file to the default styles component using the ُStyled-Component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 const App = () => {
-  return <h1>hello world!</h1>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
